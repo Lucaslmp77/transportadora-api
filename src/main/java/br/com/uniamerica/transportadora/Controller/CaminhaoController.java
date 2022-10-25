9440a43b3a5434218ca9287fdf77dc34d88f6e40
Lucaslmp77 <lucas.lmp77@gmail.com>
2022-10-25 08:16:16 -0300

    metodos CaminhaoService e CaminhaoController

--- src/main/java/br/com/uniamerica/transportadora/Controller/CaminhaoController.java
+++ src/main/java/br/com/uniamerica/transportadora/Controller/CaminhaoController.java
@@ -1,9 +1,13 @@
 package br.com.uniamerica.transportadora.Controller;
 
+import br.com.uniamerica.transportadora.Entity.Caminhao;
 import br.com.uniamerica.transportadora.Service.CaminhaoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api/caminhao")
@@ -11,5 +15,57 @@ public class CaminhaoController {
 
     @Autowired
     private CaminhaoService caminhaoService;
+
+    @PostMapping
+    public ResponseEntity<?> save(
+            @RequestBody Caminhao caminhao
+    ){
+        try{
+            this.caminhaoService.save(caminhao);
+            return ResponseEntity.ok().body("Caminhão cadastrado!");
+        }catch (RuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Caminhao>> listByAllPage(
+            Pageable pageable
+    ){
+        return ResponseEntity.ok().body(this.caminhaoService.listAll(pageable));
+    }
+
+    @GetMapping("/{idCaminhao}")
+    public ResponseEntity<Caminhao> findById(
+            @PathVariable("idCaminhao") Long idCaminhao
+    ){
+        return ResponseEntity.ok().body(this.caminhaoService.findById(idCaminhao).get());
+    }
+
+    @PutMapping("/{idCaminhao}")
+    public ResponseEntity<?> update(
+            @PathVariable Long idCaminhao,
+            @RequestBody Caminhao caminhao
+    ){
+        try{
+            this.caminhaoService.update(idCaminhao, caminhao);
+            return ResponseEntity.ok().body("Caminhão Atualizado com sucesso!");
+        }catch (RuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{idCaminhao}")
+    public ResponseEntity<?> delete(
+            @PathVariable Long idCaminhao,
+            @RequestBody Caminhao caminhao
+    ){
+        try{
+            this.caminhaoService.delete(idCaminhao, caminhao);
+            return ResponseEntity.ok().body("Caminhão deletado com sucesso!");
+        }catch (RuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 
 }
--- src/main/java/br/com/uniamerica/transportadora/Service/CaminhaoService.java
+++ src/main/java/br/com/uniamerica/transportadora/Service/CaminhaoService.java
@@ -3,8 +3,11 @@ package br.com.uniamerica.transportadora.Service;
 import br.com.uniamerica.transportadora.Entity.Caminhao;
 import br.com.uniamerica.transportadora.Repository.CaminhaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,5 +16,36 @@ public class CaminhaoService {
 
     @Autowired
     private CaminhaoRepository caminhaoRepository;
+
+    @Transactional
+    public Caminhao save(Caminhao caminhao) {
+        return this.caminhaoRepository.save(caminhao);
+    }
+
+    public Page<Caminhao> listAll(Pageable pageable) {
+        return this.caminhaoRepository.findAll(pageable);
+    }
+
+    public Optional<Caminhao> findById(Long id) {
+        return this.caminhaoRepository.findById(id);
+    }
+
+    @Transactional
+    public void update(Long id, Caminhao caminhao) {
+        if(id == caminhao.getId()) {
+            this.caminhaoRepository.save(caminhao);
+        } else {
+            throw new RuntimeException();
+        }
+    }
+
+    @Transactional
+    public void delete(Long id, Caminhao caminhao) {
+        if(id == caminhao.getId()) {
+            this.caminhaoRepository.delete(caminhao);
+        } else {
+            throw new RuntimeException();
+        }
+    }
 
 }
